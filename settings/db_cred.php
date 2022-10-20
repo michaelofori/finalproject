121872709791c86940026335493c26eb84120f80
Mike Ofori <michael.ofori@ashesi.edu.gh>
2022-10-20 00:20:24 +0000

    dcd

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -2,10 +2,10 @@
 //Database credentials
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'shoppn');
+define('DB_SERVER', 'us-cdbr-east-06.cleardb.net');
+define('DB_USERNAME', 'bcc8e00c3630df');
+define('DB_PASSWORD', 'b29c196e');
+define('DB_NAME', 'heroku_4f0f1a3c315114e');
  
 /* Attempt to connect to MySQL database */
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
@@ -16,3 +16,9 @@ if($conn === false){
 }
 
 ?>
+
+
+
+
+
+mysql://bcc8e00c3630df:b29c196e@us-cdbr-east-06.cleardb.net/heroku_4f0f1a3c315114e?reconnect=true
