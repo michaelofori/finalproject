c132c0ca0ed0d7dcae5daccd10dab607c6a441eb
Mike Ofori <michael.ofori@ashesi.edu.gh>
2022-10-30 13:56:33 +0000

    sdf

--- settings/core.php
+++ settings/core.php
@@ -6,10 +6,24 @@ session_start();
 ob_start();
 
 //funtion to check for login
-
+//funtion to check for login
+function check_login(){
+    if (!isset($_SESSION['cid'])){
+        return false;
+    }
+    return true;
+}
 
 //function to get user ID
 
+//function to get user ID
+function check_login_index(){
+    if (!isset($_SESSION['cid'])){
+        
+        return false;
+    }
+    return true;
+}
 
 //function to check for role (admin, customer, etc)
 
