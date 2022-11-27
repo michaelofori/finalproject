2d5ee1780f4218f629196065455381c44c78f31b
Mike Ofori <102876495+michaelofori@users.noreply.github.com>
2022-11-27 16:19:32 +0000

    vkjdf

--- index.php
+++ index.php
@@ -1,23 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Labs</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-</head>
-<body>
+<?php
+ header("location:view/all_product.php");
+?>
 
-<div class="container">
-  <h2>LABS</h2>
-  <a href="login/register.php" class="btn btn-info" role="button">Lab 1</a>
-  <a href="login/login.php" class="btn btn-info" role="button">Lab 2</a>
-  <!-- <button type="button" class="btn btn-info">Button</button>
-  <input type="button" class="btn btn-info" value="Input Button">
-  <input type="submit" class="btn btn-info" value="Submit Button"> -->
-</div>
 
-</body>
-</html>
+
+
+
+	
