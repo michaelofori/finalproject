f40ccb9231020ac09b8082f105ca49c33293fa21
Mike Ofori <102876495+michaelofori@users.noreply.github.com>
2022-12-01 00:10:18 +0000

    done 2

--- login/login.php
+++ login/login.php
@@ -2,7 +2,8 @@
 <html lang="en">
 
 <head>
-	<title>Login 09</title>
+	<title>Login</title>
+	<link rel="icon" href="../images/templogo.png" type="image/gif">
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -13,13 +14,12 @@
 	<link rel="stylesheet" href="css/style.css">
 
 </head>
-
 <body>
 	<section class="ftco-section">
 		<div class="container">
 			<div class="row justify-content-center">
 				<div class="col-md-6 text-center mb-5">
-					<h2 class="heading-section">Shop PN</h2>
+					<h2 class="heading-section">Kemdar Electronics</h2>
 				</div>
 			</div>
 			<div class="row justify-content-center">
@@ -28,37 +28,27 @@
 						<div class="img d-flex align-items-center justify-content-center" style="background-image: url(images/zeus.jpg);"></div>
 						<h3 class="text-center mb-0">Login</h3>
 						<p class="text-center"></p>
-
-						<form action="../actions/loginprocess.php" method="post"  class="login-form">
-
-							
-
+						<form action="../actions/loginprocess.php" method="post" class="login-form">
 							<div class="form-group">
 								<div class="icon d-flex align-items-center justify-content-center"><span class="fa fa-address-card"></span></div>
 								<input type="email" class="form-control" placeholder="email" name="customer_email" required>
 							</div>
-
 							<div class="form-group">
 								<div class="icon d-flex align-items-center justify-content-center"><span class="fa fa-key"></span></div>
-								<input type="password" class="form-control" placeholder="Password" id= "customer_pass" name="customer_pass" required>
+								<input type="password" class="form-control" placeholder="Password" id="customer_pass" name="customer_pass" required>
 							</div>
-
 							<div class="form-group">
-							<span id="wrong_pass_alert"></span>
-								<button type="submit" id="login_user" name="login_user"  
-								class="btn form-control btn-primary rounded submit px-3">Get Started</button>
+								<span id="wrong_pass_alert"></span>
+								<button type="submit" id="login_user" name="login_user" class="btn form-control btn-primary rounded submit px-3">Get Started</button>
 							</div>
 						</form>
-
 						<div class="w-100 text-center mt-4 text">
-						<?php 
-						session_start();
-                                        
-                                        if(!empty($_SESSION['error'])){
-
-                                            echo $_SESSION['error'];
-											unset($_SESSION['error']);
-                                        } ?>
+							<?php
+							session_start();
+							if (!empty($_SESSION['error'])) {
+								echo $_SESSION['error'];
+								unset($_SESSION['error']);
+							} ?>
 							<p class="mb-0">Don't have an account?</p>
 							<a href="register.php">Sign In</a>
 						</div>
@@ -67,13 +57,9 @@
 			</div>
 		</div>
 	</section>
-
 	<script src="js/jquery.min.js"></script>
 	<script src="js/popper.js"></script>
 	<script src="js/bootstrap.min.js"></script>
 	<script src="js/main.js"></script>
-
-
 </body>
-
 </html>
--- login/register.php
+++ login/register.php
@@ -2,7 +2,8 @@
 <html lang="en">
 
 <head>
-	<title>Login 09</title>
+	<title>Sign Up</title>
+	<link rel="icon" href="../images/templogo.png" type="image/gif">
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -12,6 +13,9 @@
 
 	<link rel="stylesheet" href="css/style.css">
 
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css">
+
+
 </head>
 
 <body>
@@ -19,7 +23,7 @@
 		<div class="container">
 			<div class="row justify-content-center">
 				<div class="col-md-6 text-center mb-5">
-					<h2 class="heading-section">Shop PN</h2>
+					<h2 class="heading-section">Kemdar Electronics</h2>
 				</div>
 			</div>
 			<div class="row justify-content-center">
@@ -28,54 +32,38 @@
 						<div class="img d-flex align-items-center justify-content-center" style="background-image: url(images/zeroday.jpg);"></div>
 						<h3 class="text-center mb-0">Welcome</h3>
 						<p class="text-center">Sign in by entering the information below</p>
-
-						<form action="../actions/registerprocess.php" method="post"  class="login-form">
-
+						<form action="../actions/registerprocess.php" method="post" class="login-form">
 							<div class="form-group">
 								<div class="icon d-flex align-items-center justify-content-center"><span class="fa fa-user"></span></div>
-								<input type="text" class="form-control" name="customer_name" placeholder="Full Name"  required>
+								<input type="text" class="form-control" name="customer_name" placeholder="Full Name" required>
 							</div>
-
 							<div class="form-group">
 								<div class="icon d-flex align-items-center justify-content-center"><span class="fa fa-address-card"></span></div>
 								<input type="email" class="form-control" placeholder="email" name="customer_email" required>
 							</div>
-
 							<div class="form-group">
 								<div class="icon d-flex align-items-center justify-content-center"><span class="fa fa-key"></span></div>
-								<input type="password" class="form-control" placeholder="Password" id= "customer_pass" name="customer_pass" required>
+								<input type="password" class="form-control" placeholder="Password" id="customer_pass" name="customer_pass" required>
 							</div>
-
 							<div class="form-group">
 								<div class="icon d-flex align-items-center justify-content-center"><span class="fa fa-key"></span></div>
 								<input type="password" class="form-control" placeholder="Confirm Password" id="customer_pass2" name="customer_pass2" required onkeyup="validate_password()">
 							</div>
-
 							<div class="form-group">
 								<div class="icon d-flex align-items-center justify-content-center"><span class="fa fa-globe"></span></div>
 								<input type="text" class="form-control" placeholder="country" name="customer_country" required>
 							</div>
-
 							<div class="form-group">
 								<div class="icon d-flex align-items-center justify-content-center"><span class="fa fa-paper-plane"></span></div>
 								<input type="text" class="form-control" placeholder="city" name="customer_city" required>
 							</div>
-
 							<div class="form-group">
 								<div class="icon d-flex align-items-center justify-content-center"><span class="fa fa-phone"></span></div>
 								<input type="text" class="form-control" placeholder="contact" name="customer_contact" required pattern="[0-9]{10}" onkeypress='validate(event)' oninvalid="this.setCustomValidity('Phone number should be 10 numbers')" oninput="this.setCustomValidity('')" onkeypress='validate(event)'>
 							</div>
-  
-							<!-- <div class="form-group d-md-flex">
-								<div class="w-100 text-md-right">
-									<a href="#">Forgot Password</a>
-								</div>
-							</div> -->
-
 							<div class="form-group">
-							<span id="wrong_pass_alert"></span>
-								<button type="submit" id="register_user" name="register_user" onclick="wrong_pass_alert()" 
-								class="btn form-control btn-primary rounded submit px-3">Get Started</button>
+								<span id="wrong_pass_alert"></span>
+								<button type="submit" id="register_user" name="register_user" onclick="sweet()" class="btn form-control btn-primary rounded submit px-3">Get Started</button>
 							</div>
 						</form>
 
@@ -89,60 +77,74 @@
 		</div>
 	</section>
 
+	<script>
+		function validate_password() {
+
+			var pass = document.getElementById('customer_pass').value;
+			var confirm_pass = document.getElementById('customer_pass2').value;
+			if (pass != confirm_pass) {
+				document.getElementById('wrong_pass_alert').style.color = 'red';
+				document.getElementById('wrong_pass_alert').innerHTML = '☒ Use same password';
+				document.getElementById('register_user').disabled = true;
+				document.getElementById('register_user').style.opacity = (0.4);
+			} else {
+				document.getElementById('wrong_pass_alert').style.color = 'green';
+				document.getElementById('wrong_pass_alert').innerHTML =
+					'🗹 Password Matched';
+				document.getElementById('register_user').disabled = false;
+				document.getElementById('register_user').style.opacity = (1);
+			}
+		}
+		
+		</script>
+		 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
+<script type="text/javascript">
+    function sweet() {
+        swal("Good job!", "Account created", "success");
+		timer: 200000
+    }
+    
+</script>
+<!-- <script type="text/javascript">
+		$(document).ready(function wrong_pass_alert() {
+			if (document.getElementById('customer_pass').value != "" &&
+				document.getElementById('customer_pass2').value != "") {
+					swal({
+            title: "User created!",
+            text: "Suceess message sent!!",
+            icon: "success",
+            button: "Ok",
+            timer: 20000
+        });
+		}});
+	</script> -->
 
 	<script>
-        function validate_password() {
- 
-            var pass = document.getElementById('customer_pass').value;
-            var confirm_pass = document.getElementById('customer_pass2').value;
-            if (pass != confirm_pass) {
-                document.getElementById('wrong_pass_alert').style.color = 'red';
-                document.getElementById('wrong_pass_alert').innerHTML
-                  = '☒ Use same password';
-                document.getElementById('register_user').disabled = true;
-                document.getElementById('register_user').style.opacity = (0.4);
-            } else {
-                document.getElementById('wrong_pass_alert').style.color = 'green';
-                document.getElementById('wrong_pass_alert').innerHTML =
-                    '🗹 Password Matched';
-                document.getElementById('register_user').disabled = false;
-                document.getElementById('register_user').style.opacity = (1);
-            }
-        }
- 
-        function wrong_pass_alert() {
-            if (document.getElementById('customer_pass').value != "" &&
-                document.getElementById('customer_pass2').value != "") {
-                alert("Your response is submitted");
-            } else {
-                alert("Please fill all the fields");
-            }
-        }
-    </script>
+		function validate(evt) {
+			var theEvent = evt || window.event;
 
-<script >
-            function validate(evt) {
-  var theEvent = evt || window.event;
-
-  // Handle paste
-  if (theEvent.type === 'paste') {
-      key = event.clipboardData.getData('text/plain');
-  } else {
-  // Handle key press
-      var key = theEvent.keyCode || theEvent.which;
-      key = String.fromCharCode(key);
-  }
-  var regex = /[0-9]|\./;
-  if( !regex.test(key) ) {
-    theEvent.returnValue = false;
-    if(theEvent.preventDefault) theEvent.preventDefault();
-  }
-}
-        </script>
+			// Handle paste
+			if (theEvent.type === 'paste') {
+				key = event.clipboardData.getData('text/plain');
+			} else {
+				// Handle key press
+				var key = theEvent.keyCode || theEvent.which;
+				key = String.fromCharCode(key);
+			}
+			var regex = /[0-9]|\./;
+			if (!regex.test(key)) {
+				theEvent.returnValue = false;
+				if (theEvent.preventDefault) theEvent.preventDefault();
+			}
+		}
+	</script>
 	<script src="js/jquery.min.js"></script>
 	<script src="js/popper.js"></script>
 	<script src="js/bootstrap.min.js"></script>
 	<script src="js/main.js"></script>
+
 
 
 </body>
